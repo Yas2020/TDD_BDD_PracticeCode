9b17a00e8732905e26c08581a14ae23ce0157190
Yas2020 <yas.eftekhari@gmail.com>
2024-04-19 07:24:20 +0000

    created testes to 100% coverage

--- labs/03_test_fixtures/tests/test_account.py
+++ labs/03_test_fixtures/tests/test_account.py
@@ -4,7 +4,7 @@ Test Cases TestAccountModel
 import json
 from unittest import TestCase
 from models.app import db
-from models.account import Account
+from models.account import Account, DataValidationError
 
 ACCOUNT_DATA = {}
 
@@ -66,3 +66,40 @@ class TestAccountModel(TestCase):
         self.assertEqual(result['disabled'], account.disabled)
         self.assertEqual(result['date_joined'], account.date_joined)
         self.assertEqual(result['email'], account.email)
+
+    def test_from_dict(self):
+        """ Test account from dict """
+        data = ACCOUNT_DATA[0] # get a random account
+        account = Account()
+        account.from_dict(data)
+        self.assertEqual(account.name, data["name"])
+        self.assertEqual(account.email, data["email"])
+        self.assertEqual(account.phone_number, data["phone_number"])
+        self.assertEqual(account.disabled, data["disabled"])
+
+    def test_update_an_account(self):
+        """ Test Account update using known data """
+        data = ACCOUNT_DATA[0] # get a random account
+        account = Account(**data)
+        account.create()
+        self.assertIsNotNone(account.id)
+        account.name = "Ramezon"
+        account.update()
+        found = Account.find(account.id)
+        self.assertEqual(found.name, "Ramezon")
+
+    def test_invalid_id_on_update(self):
+        """ Test invalid ID update """
+        data = ACCOUNT_DATA[1] # get a random account
+        account = Account(**data)
+        account.id = None
+        self.assertRaises(DataValidationError, account.update)
+
+    def test_delete_an_account(self):
+        """ Test Account update using known data """
+        data = ACCOUNT_DATA[0] # get a random account
+        account = Account(**data)
+        account.create()
+        self.assertEqual(len(Account.all()), 1)
+        account.delete()
+        self.assertEqual(len(Account.all()), 0)
