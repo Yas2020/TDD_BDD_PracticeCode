530f9b191963499bc75ee2f3ab601ca6f6ba3a2f
Yas2020 <yas.eftekhari@gmail.com>
2024-04-20 00:16:57 +0000

    Fixtures replaced by Factories&Fakes

--- labs/04_factories_and_fakes/tests/factories.py
+++ labs/04_factories_and_fakes/tests/factories.py
@@ -11,6 +11,7 @@ Documentation on Fuzzy Attributes:
 import factory
 from datetime import date
 from factory.fuzzy import FuzzyChoice, FuzzyDate
+import factory.fuzzy
 from models.account import Account
 
 class AccountFactory(factory.Factory):
@@ -20,3 +21,10 @@ class AccountFactory(factory.Factory):
         model = Account
 
     # Add attributes here...
+    id = factory.Sequence(lambda n: n+1)
+    name = factory.Faker("name")
+    email = factory.Faker("email")
+    phone_number = factory.Faker("phone_number")
+    disabled = FuzzyChoice(choices=[True, False])
+    date_joined = FuzzyDate(date(2020,1,1))
+
