f7714b5c69f33f54add543bfa81e7f846d19b48f
Yas2020 <yas.eftekhari@gmail.com>
2024-04-20 04:12:16 +0000

    Mocks used for testing APIs

---
+++ labs/05_mocking_objects/tests/test_imdb.py
@@ -0,0 +1,62 @@
+"""
+Test Cases for Mocking Lab
+"""
+import json
+from unittest import TestCase
+from unittest.mock import patch, Mock
+from requests import Response
+from models import IMDb
+
+IMDB_DATA = {}
+
+class TestIMDbDatabase(TestCase):
+    """Tests Cases for IMDb Database"""
+
+    @classmethod
+    def setUpClass(cls):
+        """ Load imdb responses needed by tests """
+        global IMDB_DATA
+        with open('tests/fixtures/imdb_responses.json') as json_data:
+            IMDB_DATA = json.load(json_data)
+
+
+    ######################################################################
+    #  T E S T   C A S E S
+    ######################################################################
+    
+    @patch('models.imdb.requests.get')
+    def test_search_with_no_results(self, imdb_mock):
+        """Test searching with no results"""
+        imdb_mock.return_value = Mock(status_code=404)
+        imdb = IMDb("k_12345678")
+        results = imdb.search_titles("Bambi")
+        self.assertEqual(results, {})
+
+    
+    @patch('models.imdb.requests.get')
+    def test_search_by_title_failed(self, imdb_mock):
+        """Test searching by title failed"""
+        imdb_mock.return_value = Mock(
+        spec=Response,
+        status_code=200,
+        json=Mock(return_value=IMDB_DATA["INVALID_API"])
+        )
+        imdb = IMDb("bad-key")
+        results = imdb.search_titles("Bambi")
+        self.assertIsNotNone(results)
+        self.assertEqual(results["errorMessage"], "Invalid API Key")
+
+    @patch('models.imdb.requests.get')
+    def test_movie_ratings(self, imdb_mock):
+        """Test movie Ratings"""
+        imdb_mock.return_value = Mock(
+        spec=Response,
+        status_code=200,
+        json=Mock(return_value=IMDB_DATA["GOOD_RATING"])
+        )
+        imdb = IMDb("k_12345678")
+        results = imdb.movie_ratings("tt1375666")
+        self.assertIsNotNone(results)
+        self.assertEqual(results["title"], "Bambi")
+        self.assertEqual(results["filmAffinity"], 3)
+        self.assertEqual(results["rottenTomatoes"], 5)
